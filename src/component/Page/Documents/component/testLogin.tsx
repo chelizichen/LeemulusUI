d26a1dc2ce15a15fae87700fc9c0712119ac7041
chelizichen <1347290221@qq.com>
2022-03-24 22:57:09 +0800

    验证登陆代码截图

--- src/component/Page/Documents/component/testLogin.tsx
+++ src/component/Page/Documents/component/testLogin.tsx
@@ -1,7 +1,8 @@
 import React from "react"
 import { Board } from "../../../../leemulus/Board"
 import { Title } from "../../../../leemulus/Title"
-
+import LoginTest from "../.././../../assets/code/loginTest.png"
+// 模拟 LocalStorage 登陆数据
 const store = {
     login:{
         name:'lee',
@@ -21,6 +22,23 @@ const LoginItem=(LoginData:LoginType):MethodDecorator=>{
         {
             throw new Error("登陆失败，密码不正确");
         }
+    }
+}
+
+const testLog1:MethodDecorator=(target:Object,prop:string|symbol,descriptor:PropertyDescriptor)=>{
+    const method = descriptor.value
+    descriptor.value = function()
+    {
+        console.log(' 第一次打印');
+        return method.apply(this,arguments)
+    }
+}
+const testLog2:MethodDecorator=(target:Object,prop:string|symbol,descriptor:PropertyDescriptor)=>{
+    const method = descriptor.value
+    descriptor.value = function()
+    {
+        console.log(' 第二次打印');
+        return method.apply(this,arguments)
     }
 }
 
@@ -44,9 +62,17 @@ class TestLogin extends React.Component
     {
         console.log('你好赛利亚');
     }
+
+    @testLog1
+    @testLog2
+    log123()
+    {
+        console.log('第三次打印');
+    }
     componentDidMount()
     {
         this.login();
+        this.log123()
     }
 
     render()
@@ -54,11 +80,22 @@ class TestLogin extends React.Component
         return(
             <div style={{width:'78%',marginLeft:'2%'}}>
 
-            <Title title="Decorator">使用</Title>
+            <Title title="Decorator">使用顺序</Title>
             <Board  border style={{padding:'20px',marginTop:'20px',marginBottom:'20px'}}>
                 <div style={{textIndent:'20px',marginBottom:'20px',marginTop:'20px',marginRight:'20px',fontWeight:900}}>
-
+                    函数装饰器从上至下依次出现效果
                 </div>
+            </Board>
+
+            <Title title="Decorator">验证登陆</Title>
+            <Board  border style={{padding:'20px',marginTop:'20px',marginBottom:'20px'}}>
+                <div style={{textIndent:'20px',marginBottom:'20px',marginTop:'20px',marginRight:'20px',fontWeight:900}}>
+                    将账号密码放在装饰器参数里面用来验证登陆
+                </div>
+                <div style={{textIndent:'20px',marginBottom:'20px',marginTop:'20px',marginRight:'20px',fontWeight:900}}>
+                    <img src={LoginTest}  style={{width:"100%",height:"100%",borderRadius:'10px'}} alt="decorator 使用"/>
+                </div>
+
             </Board>
             </div>
         )
--- src/component/Page/Documents/component/v_directive.tsx
+++ src/component/Page/Documents/component/v_directive.tsx
@@ -8,6 +8,24 @@ import V_use from '../../../../assets/code/v_directive_use.png'
 function VDirective(){
     return(
         <div style={{width:'78%',marginLeft:'2%'}}>
+        <Title title="VDerictive">介绍</Title>
+        <Board  border style={{padding:'20px',marginTop:'20px',marginBottom:'20px'}}>
+            <div style={{textIndent:'20px',marginBottom:'20px',marginTop:'20px',marginRight:'20px',fontWeight:900}}>
+                v-directive 用来自定义指令，可以自定义一些方法用来操作DOM，Data。
+            </div>
+            <div style={{textIndent:'20px',marginBottom:'20px',marginTop:'20px',marginRight:'20px',fontWeight:900}}>
+                v-directive 提供三个方法
+            </div>
+            <div style={{textIndent:'20px',marginBottom:'20px',marginTop:'20px',marginRight:'20px',fontWeight:900}}>
+                bind 绑定时 生效 (el,binding,v_Node)
+            </div>
+            <div style={{textIndent:'20px',marginBottom:'20px',marginTop:'20px',marginRight:'20px',fontWeight:900}}>
+                inserted 文本插入时 生效，此时真实DOM 已经呈现 (el,bindding,v_node,oldNode)
+            </div>
+            <div style={{textIndent:'20px',marginBottom:'20px',marginTop:'20px',marginRight:'20px',fontWeight:900}}>
+                Update 文本更新时生效 (el,bindding,v_node,oldNode)
+            </div>
+        </Board>
 
         <Title title="VDerictive">使用</Title>
         <Board  border style={{padding:'20px',marginTop:'20px',marginBottom:'20px'}}>
